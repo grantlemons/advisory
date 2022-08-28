6f31ea0e59f0d8213217485c1884371e18ec109b
Grant Lemons <grant0lemons@gmail.com>
2022-08-27 23:32:52 -0500

    add basic aws & neptune config

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+use aws_config;
+use aws_sdk_neptune as neptune;
+use tokio;
+
+struct AwsConfig {
+    config: aws_config::SdkConfig,
+    client: neptune::Client
+}
+
+#[tokio::main]
+async fn main() {
+    let config = aws_config::load_from_env().await;
+    let client = neptune::Client::new(&config);
+
+    let conf = AwsConfig {
+        config,
+        client,
+    };
 }
