736fabae26406ded46fe2068c692723c9bfe3e08
Grant Lemons <grant.lemons@students.alcuinschool.org>
2022-09-08 11:24:49 -0500

    change log file to log directory

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -222,7 +222,10 @@ fn setup_logger() -> Result<(), fern::InitError> {
         })
         .level(log::LevelFilter::Debug)
         .chain(std::io::stdout())
-        .chain(fern::log_file("output.log")?)
+        .chain(fern::log_file(format!(
+            "/logs/{}.log",
+            chrono::Local::now().format("[%Y-%m-%d][%H:%M]")
+        ))?)
         .apply()?;
     Ok(())
 }
