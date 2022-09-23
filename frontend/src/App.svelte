4227b9112ef07ec766e0f998b100ef33fd640cac
Grant Lemons <grant0lemons@gmail.com>
2022-09-22 21:54:36 -0500

    indent to 4 spaces

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,9 +1,9 @@
 <script>
-  import Button, { Label } from '@smui/button';
-  import Card from '@smui/card';
-  let count = 0;
+    import Button, { Label } from '@smui/button';
+    import Card from '@smui/card';
+    let count = 0;
 </script>
 
 <Button color="primary" on:click={() => count++} variant="unelevated">
-  <Label>Press To Increment: {count}</Label>
+    <Label>Press To Increment: {count}</Label>
 </Button>
