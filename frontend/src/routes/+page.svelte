a33b223031ea294d4586be41f46babace890b502
Grant Lemons <grant.lemons@students.alcuinschool.org>
2022-09-23 11:20:38 -0500

    switch to sveltekit

---
+++ frontend/src/routes/+page.svelte
@@ -0,0 +1,16 @@
+<script>
+    import Button, { Label } from '@smui/button';
+    let primary_count = 0;
+    let secondary_count = 0;
+</script>
+
+<Button color="primary" on:click={() => primary_count++} variant="unelevated">
+    <Label>Click to increment: {primary_count}</Label>
+</Button>
+<Button
+    color="secondary"
+    on:click={() => secondary_count++}
+    variant="unelevated"
+>
+    <Label>Click to increment: {secondary_count}</Label>
+</Button>
