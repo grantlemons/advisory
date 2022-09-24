97b5d553b66724950ba809ee3462580b819bb2ef
Grant Lemons <grant0lemons@gmail.com>
2022-09-24 18:12:36 -0500

    add css styling

---
+++ frontend/src/lib/Horizontal-Rule.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+    export let color: string = '#000000';
+    export let thickness: number = 1.5;
+
+    function setCssProps(node: HTMLDivElement) {
+        node.style.setProperty(`--color`, color);
+        node.style.setProperty(`--thickness`, thickness + 'px');
+        node.style.setProperty(`--radius`, thickness / 2 + 'px');
+    }
+</script>
+
+<div class="hrule" use:setCssProps />
+
+<style>
+    div {
+        background-color: var(--color);
+        height: var(--thickness);
+        width: 102%;
+        border-radius: var(--radius);
+    }
+</style>
--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,16 +1,119 @@
-<script>
+<script lang="ts">
     import Button, { Label } from '@smui/button';
-    let primary_count = 0;
-    let secondary_count = 0;
+    import Textfield from '@smui/textfield';
+
+    import HRule from '../lib/Horizontal-Rule.svelte';
+
+    let email = '';
+    let password = '';
+
+    function sign_in() {}
+    function sign_up() {}
+    function sign_w_google() {}
 </script>
 
-<Button color="primary" on:click={() => primary_count++} variant="unelevated">
-    <Label>Click to increment: {primary_count}</Label>
-</Button>
-<Button
-    color="secondary"
-    on:click={() => secondary_count++}
-    variant="unelevated"
->
-    <Label>Click to increment: {secondary_count}</Label>
-</Button>
+<div class="right">
+    <div class="content">
+        <div class="input">
+            <Textfield
+                style="width: 100%"
+                variant="outlined"
+                bind:value={email}
+                label="Email"
+            />
+            <Textfield
+                style="width: 100%"
+                variant="outlined"
+                bind:value={password}
+                label="Password"
+            />
+        </div>
+
+        <div class="buttons">
+            <div class="button signin">
+                <Button
+                    color="secondary"
+                    on:click={sign_in}
+                    variant="unelevated"
+                >
+                    <Label>Sign In</Label>
+                </Button>
+            </div>
+
+            <HRule color="#939393" thickness={2} />
+
+            <div class="button google">
+                <Button
+                    color="secondary"
+                    on:click={sign_w_google}
+                    variant="unelevated"
+                >
+                    <Label>Sign In With Google</Label>
+                </Button>
+            </div>
+
+            <!-- LARGER SPACE -->
+
+            <div class="button signup">
+                <Button
+                    color="secondary"
+                    on:click={sign_up}
+                    variant="unelevated"
+                >
+                    <Label>Sign Up</Label>
+                </Button>
+            </div>
+        </div>
+    </div>
+</div>
+
+<style>
+    .right {
+        position: absolute;
+        display: flex;
+        justify-content: center;
+
+        align-items: center;
+        margin-left: 50%;
+        height: 100vh;
+        width: 50vw;
+    }
+    .content {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        row-gap: 20px;
+
+        align-items: center;
+        width: 30%;
+    }
+    .input {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        row-gap: 10px;
+
+        align-items: center;
+        width: 100%;
+    }
+    .buttons {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        row-gap: 10px;
+
+        align-items: center;
+        width: 100%;
+    }
+    .button {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        row-gap: 10px;
+
+        width: 100%;
+    }
+    .signup {
+        margin-top: 30px;
+    }
+</style>
