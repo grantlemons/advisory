b54548dc3d2ba4fed9c402d3de4d4070ce2f176b
Grant Lemons <grant0lemons@gmail.com>
2023-01-23 01:00:58 -0600

    remove user_id from settings struct

--- backend/src/lib/settings.rs
+++ backend/src/lib/settings.rs
@@ -4,10 +4,6 @@ use serde::{Deserialize, Serialize};
 /// Form for [`crate::advisories::Advisory`]'s input
 #[derive(Deserialize, Serialize, Debug)]
 pub(crate) struct Settings {
-    /// The ID of the user's account within the database.
-    ///
-    /// Can be based on different things, like auth cred
-    pub(crate) user_id: String,
     /// The respective value of each factor in the calculation of advisory 'scores'
     pub(crate) weights: Weights,
     /// Number of advisories to be generated
@@ -16,6 +12,6 @@ pub(crate) struct Settings {
 
 impl crate::Verify for Settings {
     fn verify(&self) -> bool {
-        !self.user_id.is_empty() && self.weights.verify() && self.num_advisories > 0
+        self.weights.verify() && self.num_advisories > 0
     }
 }
