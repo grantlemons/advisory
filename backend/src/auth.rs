87c1dd3a013891af744dba32f1b40f9c29126a70
Grant Lemons <grant0lemons@gmail.com>
2023-01-22 21:51:27 -0600

    add backend auth layer

---
+++ backend/src/auth.rs
@@ -0,0 +1,67 @@
+use crate::SharedState;
+use jsonwebtokens_cognito;
+use serde::{Deserialize, Serialize};
+
+#[derive(Deserialize, Serialize, Debug)]
+pub struct UserData {
+    pub aud: String,
+    pub auth_time: u32,
+    pub email: String,
+    pub email_verified: bool,
+    pub event_id: String,
+    pub exp: u32,
+    pub iat: u32,
+    pub iss: String,
+    pub jti: String,
+    pub name: String,
+    pub origin_jti: String,
+    pub sub: String,
+    pub token_use: String,
+}
+
+use axum::{
+    extract::State,
+    http::{Request, StatusCode},
+    middleware::Next,
+    response::Response,
+};
+
+pub async fn auth<B>(
+    State(state): State<SharedState>,
+    req: Request<B>,
+    next: Next<B>,
+) -> Result<Response, StatusCode> {
+    let auth_header = req
+        .headers()
+        .get(axum::http::header::AUTHORIZATION)
+        .and_then(|header| header.to_str().ok());
+
+    match auth_header {
+        Some(auth_header) if verify_jwt(auth_header, state).await => Ok(next.run(req).await),
+        _ => Err(StatusCode::UNAUTHORIZED),
+    }
+}
+
+pub async fn verify_jwt(token: &str, state: SharedState) -> bool {
+    match decrypt_jwt(token, &state.keyset, &state.verifier).await {
+        Ok(user) => {
+            log::info!("Successful JWT Verification for user {}", user.name);
+            true
+        }
+        Err(_) => {
+            log::info!("Failed JWT Verification");
+            false
+        }
+    }
+}
+
+pub async fn decrypt_jwt(
+    token: &str,
+    keyset: &jsonwebtokens_cognito::KeySet,
+    verifier: &jsonwebtokens::Verifier,
+) -> Result<UserData, jsonwebtokens_cognito::Error> {
+    match keyset.verify(&token, verifier).await {
+        Ok(res) => Ok(serde_json::from_value(res).unwrap()),
+        Err(err) => Err(err),
+    }
+}
