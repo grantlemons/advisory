7051ea098d84283e3ca69c72a6f26d449ce3e2c7
Grant Lemons <grant0lemons@gmail.com>
2023-01-23 18:54:00 -0600

    remove img css

--- frontend/src/lib/Logo.svelte
+++ frontend/src/lib/Logo.svelte
@@ -5,21 +5,8 @@
 </script>
 
 {#if name}
-    <!-- <img src="" alt="Project Logo" /> -->
     <Name />
 {:else}
     <!-- <img src="" alt="Project Logo" /> -->
 {/if}
 
-<style>
-    img {
-        background-color: #939393;
-        border-radius: 30px;
-        padding: 40px;
-        padding-left: 80px;
-        padding-right: 80px;
-        width: 100%;
-        font-family: Roboto;
-        font-size: 200%;
-    }
-</style>
