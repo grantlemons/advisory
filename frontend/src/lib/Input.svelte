25ff8469a815c39cbfb6c526dcbfec1ef6cdd940
Grant Lemons <grant0lemons@gmail.com>
2023-01-29 14:17:43 -0600

    disabled option for input

--- frontend/src/lib/Input.svelte
+++ frontend/src/lib/Input.svelte
@@ -5,11 +5,13 @@
     export let label: string = 'Default Label';
     export let password: boolean = false;
     export let error_text: string = '';
+    export let disabled: boolean = false;
 </script>
 
 <TextField
     style="width: 100%"
     variant="outlined"
+    {disabled}
     type={password ? 'password' : 'text'}
     invalid={error_text != ''}
     bind:value
