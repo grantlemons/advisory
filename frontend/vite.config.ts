b9641edb9ef38dd6a261c9b08a1ef7b0b64a2f16
Grant Lemons <grant0lemons@gmail.com>
2023-02-17 14:22:57 -0600

    proxy frontend -> backend

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,6 +1,26 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
-export default defineConfig({
-    plugins: [sveltekit()],
+export default defineConfig(({ mode }) => {
+    if (mode == 'development') {
+        return {
+            plugins: [sveltekit()],
+            server: {
+                proxy: {
+                    '/api': {
+                        target: 'https://127.0.0.1:81',
+                        changeOrigin: true,
+                        secure: false,
+                    },
+                },
+                watch: {
+                    usePolling: true,
+                },
+            },
+        };
+    } else {
+        return {
+            plugins: [sveltekit()],
+        };
+    }
 });
