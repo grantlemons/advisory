c687c3fd3d7df947a96a348094474cb3156fb30a
Grant Lemons <grant0lemons@gmail.com>
2023-02-26 18:50:11 -0600

    add list and card

--- frontend/src/lib/Advisory.svelte
+++ frontend/src/lib/Advisory.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
     import type { Advisory, Student, Teacher } from '$lib/DBTypes';
+    import Card, { Content } from '@smui/card';
+    import List, { Item, Separator, Text } from '@smui/list';
     export let data: Advisory;
 
     let teachers: Teacher[] = data.advisors;
@@ -7,17 +9,31 @@
 </script>
 
 <div class="advisory-card">
-    ({teachers.map((t) => t.name).join(', ')})
-    {#each students as student}
-        <div>
-            {student.name}
-        </div>
-    {/each}
+    <Card>
+        <Content>
+            <List nonInteractive>
+                {#each teachers as teacher}
+                    <Item>
+                        <Text>{teacher.name}</Text>
+                    </Item>
+                {/each}
+            </List>
+            <Separator />
+            <List nonInteractive>
+                {#each students as student}
+                    <Item>
+                        <Text>{student.name}</Text>
+                    </Item>
+                {/each}
+            </List>
+        </Content>
+    </Card>
 </div>
 
 <style>
     .advisory-card {
-        border-radius: 12px;
-        background-color: rgb(199, 199, 199);
+        font-family: Roboto;
+        /* border-radius: 12px; */
+        /* background-color: rgb(199, 199, 199); */
     }
 </style>
